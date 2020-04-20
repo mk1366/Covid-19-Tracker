0abcbcd742e6134c278039ec6be12a439c0494e2
Mahelet.K <maheletkassa1@gmail.com>
2020-04-20 16:17:06 -0400

    Created Card, Chart, CountryPicker with besic functionality to render on App.js and created a functinality to fetch data from the third party api with axios

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+
+class App extends React.Component {
+
+   async componentDidMount() {
+      const data = await fetchData();
+      console.log(data);
+   }
+   
+   render() {
+      return (
+         <div className={styles.container}>
+            <Cards />
+            <CountryPicker />
+            <Chart />
+         </div>
+      )
+   }
+}
+
+export default App;
---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+// make function that will fetch some data
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async() => {
+   try {
+      const response = await axios.get(url);
+
+      return response;
+   }  catch (error){
+
+
+   }
+}
