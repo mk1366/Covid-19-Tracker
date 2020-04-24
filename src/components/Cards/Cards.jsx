9bb39ba04a0b9453bfc50d993a566f35fc086572
Mahelet.K <maheletkassa1@gmail.com>
2020-04-24 18:24:39 -0400

    installed and import chart.js, created a functionality in chart.js to fetch the data and display the chart

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
-import styles from './Cards.module.css';
 import CountUp from 'react-countup';
-import cx from 'Classnames';
+import cx from 'classnames';
+
+import styles from './Cards.module.css';
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
    if (!confirmed) {
@@ -11,7 +12,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
    return (
       <div className={styles.container}>
          <Grid container spacing={3} justify="center">
-            <Grid item component={Card}> xs={12} md{3}className={cx(styles.card, styles.confirmed)}
+         <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
                <CardContent>
                   <Typography color="textSecondary" gutterBottom>Infected</Typography>
                   <Typography variant="h5">
@@ -22,7 +23,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                   <Typography variant="body2">Number of active cases of COVID-19</Typography>
                </CardContent>
             </Grid>
-            <Grid item component={Card}> xs={12} md{3}className={cx(styles.card, styles.recovered)}
+            <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.recovered)}>
                <CardContent>
                   <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                   <Typography variant="h5">
@@ -33,7 +34,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                   <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                </CardContent>
             </Grid>
-            <Grid item component={Card}> xs={12} md{3}className={cx(styles.card, styles.deaths)}
+            <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.deaths)}>
                <CardContent>
                   <Typography color="textSecondary" gutterBottom>Death</Typography>
                   <Typography variant="h5">
