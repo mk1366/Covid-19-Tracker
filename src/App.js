fd1bfadbe8cc08f6c73dc8b7deb2d41b80496a80
Mahelet.K <maheletkassa1@gmail.com>
2020-04-27 22:57:47 -0400

    created fetchedData function that is a async to return only the data I need and fetched the country from the Api and looping over the contry once I get the data setting to the State to be able to chose just one country

--- src/App.js
+++ src/App.js
@@ -5,30 +5,38 @@ import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import coronaImage from './images/image.png';
 
 
 class App extends React.Component {
    state = {
-      data: {},
-      }
+     data: {},
+     country: '',
+    }
 
    async componentDidMount() {
-      const fetchdata = await fetchData();
+      const fetchedData = await fetchData();
 
-      console.log(fetchdata)
 
-      this.setState({ data: fetchdata });
+      this.setState({ data: fetchedData });
    }
    
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+
+  }
    render() {
-      const { data } = this.state;
-      return (
-         <div className={styles.container}>
-            <Cards data={data} />
-            <CountryPicker />
-            <Chart />
-         </div>
-      )
+      const { data, country } = this.state;
+     return (
+       <div className={styles.container}>
+         <img clasName={styles.image} src={coronaImage} alt="COVID-19"/>
+         <Cards data={data} />
+         <CountryPicker handleCountryChange={this.handleCountryChange} />
+         <Chart data={data} country={country} />
+       </div>
+     );
    }
 }
 
